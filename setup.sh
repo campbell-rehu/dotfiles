c1d8d65b1a3039bb285adb29f33b8fbbec448985
Campbell <campbellrehu@me.com>
2023-06-15 06:55:16 +1200

    Add option to dryRun run script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+dryRun="$1"
 curl -O https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh
 chmod +x ./run.sh
-./run.sh $PWD/config ~/.config dryRun
+./run.sh $PWD/config ~/.config $dryRun
