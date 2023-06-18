b96f1c99569c59573cef6e3ac732c78ad7f1d7d5
Campbell <campbellrehu@me.com>
2023-06-18 20:57:06 +1200

    Add custom-config.fish as file to copy

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 # Check for --dry-run argument or invalid arguments
 if [[ $1 == "--dry-run" ]]; then
     echo "Running in dry-run mode, no changes will be made."
-    curl -sS https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh | bash -s -- "$1" $PWD/config ~/.config $PWD/config/lvim/custom-config.lua
+    curl -sS https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh | bash -s -- "$1" $PWD/config ~/.config $PWD/config/lvim/custom-config.lua $PWD/config/fish/custom-config.fish
 elif [[ -n $1 ]]; then
     echo "Error: Invalid argument. Use --dry-run for dry run mode."
     exit 1
 else
     echo "Running in normal mode."
-    curl -sS https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh | bash -s -- $PWD/config ~/.config $PWD/config/lvim/custom-config.lua
+    curl -sS https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh | bash -s -- $PWD/config ~/.config $PWD/config/lvim/custom-config.lua $PWD/config/fish/custom-config.fish
 fi
