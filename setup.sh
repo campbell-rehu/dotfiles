e05261fb519bbc44a319f4988e89dd18f8212574
Campbell <campbellrehu@me.com>
2024-03-25 15:44:01 +1300

    setup.sh: refactor;alacritty: add custom.toml

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
-# Check for --dry-run argument or invalid arguments
-if [[ $1 == "--dry-run" ]]; then
-    echo "Running in dry-run mode, no changes will be made."
-    curl -sS https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh | bash -s -- "$1" $PWD/config ~/.config $PWD/config/lvim/custom-config.lua $PWD/config/fish/custom-config.fish
-elif [[ -n $1 ]]; then
-    echo "Error: Invalid argument. Use --dry-run for dry run mode."
-    exit 1
-else
-    echo "Running in normal mode."
-    curl -sS https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh | bash -s -- $PWD/config ~/.config $PWD/config/lvim/custom-config.lua $PWD/config/fish/custom-config.fish
+CUSTOM_CONFIG_FILE_PATHS=($PWD/config/lvim/custom-config.lua $PWD/config/fish/custom-config.fish $PWD/config/alacritty/custom.toml)
+
+if [[ $# -ge 1 ]]; then
+    if [[ $1 != "--dry-run" ]]; then
+        echo "Error: Invalid argument. Use --dry-run for dry run mode."
+        echo "Usage: ./setup.sh [--dry-run]"
+        exit 1
+    fi
+    curl -sS https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh | bash -s -- "$1" $PWD/config ~/.config echo ${CUSTOM_CONFIG_FILE_PATHS[*]}
 fi
+echo "$1"
+curl -sS https://raw.githubusercontent.com/campbell-rehu/sym-link-test/main/run.sh | bash -s -- $PWD/config ~/.config echo ${CUSTOM_CONFIG_FILE_PATHS[*]}
