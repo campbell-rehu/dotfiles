495f48da1415d0c6cd538710850279a59b5c2730
Campbell <campbellrehu@me.com>
2024-04-15 22:38:27 +1200

    nvim: generateKeymap allow passing table as options

--- config/nvim/lua/campbellrehu/core/helpers.lua
+++ config/nvim/lua/campbellrehu/core/helpers.lua
@@ -1,7 +1,13 @@
 local helpers = {}
 
-function helpers.generateKeymap(mode, keymap, command, description)
-  vim.keymap.set(mode, keymap, command, { desc = description })
+function helpers.generateKeymap(mode, keymap, command, opts)
+  local options = {}
+  if type(opts) ~= "table" then
+    options.desc = opts
+  else
+    options = opts
+  end
+	vim.keymap.set(mode, keymap, command, options)
 end
 
 return helpers
