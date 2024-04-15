132883bd312b5aafec0ef43196f529d72b5ead3e
Campbell <campbellrehu@me.com>
2024-04-15 22:38:44 +1200

    nvim: add lsp module to lazy setup

--- config/nvim/lua/campbellrehu/lazy.lua
+++ config/nvim/lua/campbellrehu/lazy.lua
@@ -11,4 +11,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("campbellrehu.plugins")
+require("lazy").setup({ { import = "campbellrehu.plugins" }, { import = "campbellrehu.plugins.lsp" }  }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
