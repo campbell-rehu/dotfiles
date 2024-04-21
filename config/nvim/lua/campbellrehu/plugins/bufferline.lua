27bf05b01401989bc862b5d7c10fe281ba1fcab2
Campbell <campbellrehu@me.com>
2024-04-21 22:55:25 +1200

    nvim: bufferline - use buffers instead of tabs

--- config/nvim/lua/campbellrehu/plugins/bufferline.lua
+++ config/nvim/lua/campbellrehu/plugins/bufferline.lua
@@ -1,10 +1,10 @@
 return {
-  'akinsho/bufferline.nvim', 
-  version = "*", 
+  'akinsho/bufferline.nvim',
+  version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   opts = {
     options = {
-      mode = "tabs",
+      mode = "buffers",
       separator_style = "slant",
       offsets = {
         {
