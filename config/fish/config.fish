006817f21fbfe9730055fdec61c393297132dea0
campbell.rehu <campbell.rehu@xero.com>
2024-05-02 10:29:36 +1200

    fish: set fish_vi_key_bindings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,16 @@
 if status is-interactive
-	# Source cargo for rust development 
-	bass source ~/.cargo/env 
+    # Source cargo for rust development 
+    bass source ~/.cargo/env
 end
 
 alias vim="lvim"
 
 alias edit-fish="lvim ~/.config/fish/config.fish"
-alias refresh-fish="source ~/.config/fish/config.fish" 
+alias refresh-fish="source ~/.config/fish/config.fish"
 
 set -U fish_greeting
+
+set -g fish_key_bindings fish_vi_key_bindings
 
 # Use the custom-config.fish file for additional fish config options
 source ~/.config/fish/custom-config.fish
