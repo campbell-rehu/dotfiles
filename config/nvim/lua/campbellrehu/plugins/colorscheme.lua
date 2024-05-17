a4d8c0615d0034f9ebbdd29f6da55abfa4fbea9a
Campbell <campbellrehu@me.com>
2024-05-17 21:12:34 +1200

    nvim: add more colorschemes

--- config/nvim/lua/campbellrehu/plugins/colorscheme.lua
+++ config/nvim/lua/campbellrehu/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({ flavour = "macchiato" })
-		vim.cmd("colorscheme catppuccin")
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({ flavour = "macchiato" })
+			vim.cmd("colorscheme catppuccin")
+		end,
+	},
+	{
+		"xero/miasma.nvim",
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		config = function()
+			require("tokyonight").setup({ style = "storm" })
+		end,
+	},
 }
