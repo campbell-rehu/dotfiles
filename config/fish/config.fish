1a054decdc7dd2a7549158aa61e8b61138396bea
Campbell Rehu <campbellrehu@me.com>
2024-06-11 21:36:51 +1200

    fish: reinstate tide

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,9 +5,7 @@ end
 
 alias edit-fish="lvim ~/.config/fish/config.fish"
 alias refresh-fish="source ~/.config/fish/config.fish"
-
 set -U fish_greeting
-
 set -g fish_key_bindings fish_vi_key_bindings
 
 set -gx EDITOR vim
