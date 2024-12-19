68e4fc375429c2c2173067f22fa656160f9a6892
Campbell <campbellrehu@me.com>
2024-12-19 15:47:48 +1300

    nvim: update telescope

--- config/nvim/lua/campbellrehu/plugins/telescope.lua
+++ config/nvim/lua/campbellrehu/plugins/telescope.lua
@@ -1,10 +1,13 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
+	tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
