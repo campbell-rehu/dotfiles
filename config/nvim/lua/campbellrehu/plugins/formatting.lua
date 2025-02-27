94145c9f6d843a6cdbc7142443bc0e49723991af
campbell.rehu <campbell.rehu@xero.com>
2025-02-28 12:08:37 +1300

    nvim: formatting.lua
    - Add custom commands to Enable/Disable formatting on save

--- config/nvim/lua/campbellrehu/plugins/formatting.lua
+++ config/nvim/lua/campbellrehu/plugins/formatting.lua
@@ -1,39 +1,59 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettierd" },
-				typescript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				svelte = { "prettierd" },
-				css = { "prettierd" },
-				html = { "prettierd" },
-				json = { "prettierd" },
-				yaml = { "prettierd" },
-				markdown = { "prettierd" },
-				graphql = { "prettierd" },
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				go = { "gofumpt", "goimports", "golines" },
-				bash = { "shfmt", "beautysh", "shellcheck", "shellharden" },
-				csharp = { "csharpier" },
-				fish = { "fish_indent" },
-				sql = { "sql_formatter" },
-				terraform = { "terraform_fmt" },
-				["*"] = { "codespell" },
-			},
-		})
-		require("conform").setup({
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		})
-	end,
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        svelte = { "prettierd" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        graphql = { "prettierd" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        go = { "gofumpt", "goimports", "golines" },
+        bash = { "shfmt", "beautysh", "shellcheck", "shellharden" },
+        csharp = { "csharpier" },
+        fish = { "fish_indent" },
+        sql = { "sql_formatter" },
+        terraform = { "terraform_fmt" },
+        ["*"] = { "codespell" },
+      },
+    })
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
+    require("conform").setup({
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
+    })
+  end,
 }
